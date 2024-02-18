a139a126da47d8fbaf8ceece3a6ae766880aa8df
Abdo <abdo@abdnh.net>
2024-02-18 16:27:00 +0300

    Fix recording of last card not cleared properly in some cases

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 import os
 import sys
-from typing import List, Literal, Tuple
+from typing import List, Tuple
 
 from anki.cards import Card
 from anki.hooks import wrap
 from aqt import mw
 from aqt.browser.previewer import Previewer
 from aqt.gui_hooks import (
-    reviewer_did_answer_card,
+    reviewer_did_show_question,
     reviewer_will_show_context_menu,
     state_shortcuts_will_change,
 )
@@ -42,11 +42,9 @@ def add_state_shortcut(state: str, shortcuts: List[Tuple[str, Callable]]) -> Non
         )
 
 
-def clear_last_recording_reference(
-    reviewer: Reviewer, card: Card, ease: Literal[1, 2, 3, 4]
-) -> None:
+def clear_last_recording_reference(card: Card) -> None:
     # Clear reference of the last recording of the answered card so that a recording only plays on its card
-    reviewer._recordedAudio = None
+    mw.reviewer._recordedAudio = None
 
 
 def add_previewer_shortcut(previewer: Previewer) -> None:
@@ -59,5 +57,5 @@ def add_previewer_shortcut(previewer: Previewer) -> None:
 patch_recording()
 reviewer_will_show_context_menu.append(add_menu_item)
 state_shortcuts_will_change.append(add_state_shortcut)
-reviewer_did_answer_card.append(clear_last_recording_reference)
+reviewer_did_show_question.append(clear_last_recording_reference)
 Previewer.open = wrap(Previewer.open, add_previewer_shortcut, "after")  # type: ignore
